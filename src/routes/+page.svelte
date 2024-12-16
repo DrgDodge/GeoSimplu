56eea22e7b188ff770803c8f77b0840eccaad919
DrgDodge <dragonul530@gmail.com>
2024-12-17 00:04:19 +0200

    V0.16 - Update Info + Navbar to contain EU lessons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="flex items-center justify-center p-5 -mt-20 fade-in">
     <div class="text-center">
         <p class="text-6xl font-bold text-white text-outline">Invata simplu si rapid</p>
-        <p class="text-2xl font-bold text-white text-outline">Acest site vă prezintă *Lectie Istorie* și, în viitor, toate lecțiile de istorie din liceu.</p>
+        <p class="text-2xl font-bold text-white text-outline">Acest site vă prezintă Uniunea Europeana</p>
     </div>
     <img src="/images/Logo.png" alt="vulture" class="w-1/4 h-auto ml-5 my-auto">
 </div>

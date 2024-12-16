e28ab7c89eff20830ef992a800e939b5d0eb60b4
DrgDodge <dragonul530@gmail.com>
2024-12-16 23:25:09 +0200

    V0.13 - ResponsiveSite + Optimized Independent Bk Image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
     import { page } from '$app/stores';
     import "/src/app.css";
     import { fade } from 'svelte/transition';
+    import Device from 'svelte-device-info';
 
 
 //     onNavigate((navigation) => {
@@ -15,12 +16,17 @@
 // 		});
 // 	});
 // });
+
+
+
+
 </script>
 
+<meta name="viewport" content="width=device-width, initial-scale=0.7">
 
 
-<div class="bg-cover bg-center h-screen bg-[url('https://4kwallpapers.com/images/wallpapers/lakeside-sunrise-early-morning-minimal-art-gradient-3840x2160-4587.png')]">
-    <nav class="absolute top-10 left-1/2 transform -translate-x-1/2 backdrop-blur-sm bg-white/30 text-2xl text-white font-bold flex rounded-3xl z-50 p-3">
+<div class="bg-cover bg-center h-screen bg-[url('/images/bkuc.webp')]">
+    <nav class="absolute top-10 left-1/2 transform -translate-x-1/2 backdrop-blur-sm bg-white/30 text-3xl text-white font-bold flex rounded-3xl z-10 p-3">
         
         <ul class="flex space-x-10">
             <li>
@@ -48,7 +54,7 @@
             </li>
 
             <li>
-                <a href="/about" class="hover:text-blue-800" class:active={$page.url.pathname === '/about'}>Despre Noi</a>
+                <a href="/about" class="hover:text-blue-800" class:active={$page.url.pathname === '/about'}>Info</a>
             </li>
             <li>
                 <a href="/contact" class="hover:text-blue-800" class:active={$page.url.pathname === '/contact'}>Contact</a>
@@ -56,12 +62,20 @@
         </ul>
     </nav>
 
+    <div>  </div>
+    <div>  </div>
+    <div>  </div>
+    <div>  </div>
+    <div>  </div>
+
     <div class="pt-28 pb-20">
         <slot />
     </div>
 </div>
 
+
+
 <footer class="fixed bottom-4 left-1/2 transform -translate-x-1/2 backdrop-blur-md bg-white/20 text-white text-center py-2 px-6 z-50 rounded-2xl">
-    <p class="text-sm font-bold">© istoria.top - Toate drepturile rezervate  Alpha V2.0</p>
+    <p class="text-l font-bold">© istoria.top - Toate drepturile rezervate  Alpha V2.0</p>
 </footer>
 

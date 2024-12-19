b48b2fe1db49fb7980012fc14755f684f607aa40
DrgBest <dragonul530@gmail.com>
2024-12-19 11:54:42 +0200

    V0.19 - Test lesson thanks to @Zaque-69

--- src/routes/lesson1/+page.svelte
+++ src/routes/lesson1/+page.svelte
@@ -1,5 +1,29 @@
-<p class="text-5xl text-white font-bold">Lesson1</p>
+<div class="flex items-center justify-center flex-wrap p-5 -mt-20 fade-in">
+    <div class="text-center min-w-96 max-w-4xl w-full" >
+        <p class="text-6xl font-bold text-white text-outline">Constituirea UE</p>
+        <p class="text-2xl text-white text-outline">
+            
+        </p>
+        <p class="text-2xl text-white mt-5 text-outline px-20">
+            UE este o organizatie supra-statala formata din 27 de state. Punctul de plecare in constituirea uniunii europene l-a reprezentat declaratia Schumann de la data de 9 mai 1950 prin care statele Europei de Vest erau invitate sa isi uneasca industriile carbunelui si otelului. 
+        </p>
+    </div>
+    <img src="/images/Logo.png" alt="vulture" class="w-1/4 h-auto ml-5 my-auto flex-wrap">
+</div>
 
+<style>
+    *{
+        border : 1px solid red;
+    }
+    /* #vulture {
+        min-width : 5rem;
+    }
+    #mw {
+        min-width : 25rem;
+        max-width : 50rem;
+        width : 100%;
+    } */
+</style>
 
 <script>
     import "/src/app.css"

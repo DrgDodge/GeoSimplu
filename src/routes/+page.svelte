af47ce27207768a1b2f695e3fe4cec64d9154460
DrgDodge <dragonul530@gmail.com>
2025-02-27 20:30:05 +0200

    Revert

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
         <p class="text-6xl font-bold text-white text-outline">EuroSimplu - Invata simplu si rapid</p>
         <p class="text-2xl font-bold text-white text-outline">Acest site vă prezintă Uniunea Europeana</p>
     </div>
-    <img src="https://cdn.discordapp.com/attachments/1344408488587300874/1344432807098847325/WhatsApp_Image_2025-02-27_at_00.15.52_1939ff40.jpg?ex=67c18cdf&is=67c03b5f&hm=711fad46e0491f3d790f009517fb91bc717839d920b96734f47ae4cb03d77f10&" alt="vulture" class="w-1/4 h-auto ml-5 my-auto">
+    <img src="/images/Logo.png" alt="vulture" class="w-1/4 h-auto ml-5 my-auto">
 </div>
 
 <script>
